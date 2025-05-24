3588b4068b9ee172f04b7ec18b00974ced1875e0
chosun-nhn32 <cj@>
2025-05-24 10:10:10 +0900

    feat : Home페이지 logo img 추가와 Button UI

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,31 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from 'styled-components';
 
 const StBox = styled.div`
   width: 100vw;
   height: 100vh;
   margin: 20px;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const StButton = styled.button`
+  background-color: red;
+  color: white;
+  border: none;
+  width: 150px;
+  height: 50px;
+  font-size: 15px;
+  border-radius: 10px;
+`;
+
+const StImg = styled.img`
+  width: 300px;
+  height: 100px;
+  margin: 30px;
 `;
 
 const Home = () => {
@@ -17,13 +34,14 @@ const Home = () => {
   return (
     <>
       <StBox>
-        <button
+        <StImg src={'/static/pokemon.png'}></StImg>
+        <StButton
           onClick={() => {
-            navigate("/dex");
+            navigate('/dex');
           }}
         >
           포켓몬 도감 시작하기
-        </button>
+        </StButton>
       </StBox>
     </>
   );
